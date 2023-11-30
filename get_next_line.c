231f16f22546b477920e5991e05c03f4914a398e
btaveira <btaveira.r@gmail.com>
2023-11-30 17:00:15 -0300

    gnl final version without bonus

--- get_next_line.c
+++ get_next_line.c
@@ -6,44 +6,96 @@
 /*   By: btaveira <btaveira@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:03:11 by btaveira          #+#    #+#             */
-/*   Updated: 2023/11/23 19:00:23 by btaveira         ###   ########.fr       */
+/*   Updated: 2023/11/29 19:19:08 by btaveira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*ft_free(char *str, char *newstr)
 {
-	int	bytes_read;
-	static char	*line;
-	
-	line = ft_calloc(BUFFER_SIZE + 1,sizeof(char));
-	if(!line)
+	char	*temp;
+
+	temp = ft_strjoin(str, newstr);
+	free(str);
+	return (temp);
+}
+
+char	*ft_cut_line(char *str)
+{
+	size_t	len;
+	char	*line;
+
+	len = 0;
+	while (str[len] && str[len] != '\n')
+		len++;
+	if (str[len] == '\n')
+		len++;
+	line = (char *)malloc(sizeof(char) * len + 1);
+	if (!line)
 		return (NULL);
-	bytes_read = read(fd, line, BUFFER_SIZE);
-	while(bytes_read != 0)
+	line[len] = '\0';
+	while (len--)
+		line[len] = str[len];
+	if (line[0] == '\0')
 	{
-		
-	}
-	line[bytes_read] = '\0';
-	if(bytes_read <= 0)
+		free(line);
 		return (NULL);
+	}
 	return (line);
 }
 
-int	main(void)
+char	*ft_delete_line(char *str)
 {
-	int    fd;
-	char  *next_line;
-	int  count;
+	int	len;
+	char	*line_to_delete;
 
-	count = 0;
-	fd = open("example.txt", O_RDONLY);
-	next_line = get_next_line(fd);
-	count++;
-	printf("[%d]:%s\n", count, next_line); //count is to show you the line numbers
- 	next_line = NULL;
+	len = 0;
+	while (str[len] && str[len] != '\n')
+		len++;
+	if (str[len] == '\n')
+		len++;
+	if (str[len] == '\0')
+	{
+		free(str);
+		return (NULL);
+	}
+	line_to_delete = ft_strdup(str + len);
+	free(str);
+	return (line_to_delete);
+}
 
-	close(fd);
-	return (0);
+char	*ft_reader(int fd)
+{
+	int		bytes_read;
+	static char	*str;
+	char		*line;
+	char		*newstr;
+
+	bytes_read = 1;
+	newstr = (char *)malloc((BUFFER_SIZE + 1 ) * sizeof(char));
+	if (!newstr)
+		return (NULL);
+	while (bytes_read > 0 && (!str || !ft_strchr(str,'\n')))
+	{
+		bytes_read = read(fd, newstr, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(newstr);
+			return (NULL);
+		}
+		newstr[bytes_read] = '\0';
+		str = ft_free(str, newstr);
+	}
+	free(newstr);
+	line = ft_cut_line(str);
+	str = ft_delete_line(str);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	return (ft_reader(fd));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,71 +6,77 @@
 /*   By: btaveira <btaveira@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 12:04:20 by btaveira          #+#    #+#             */
-/*   Updated: 2023/11/23 15:36:44 by btaveira         ###   ########.fr       */
+/*   Updated: 2023/11/29 19:37:23 by btaveira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-/*char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1,char *s2)
 {
 	char	*str;
-	size_t	index_s1;
-	size_t	index_s2;
-	size_t	len_s1;
-	size_t	len_s2;
+	size_t	index;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	str = malloc(sizeof (char) * (len_s1 + len_s2) + 1);
+	if (!s1 && !s2)
+		return (NULL);
+	str = malloc(sizeof (char) * (ft_strlen(s1) + ft_strlen(s2)) + 1);
 	if (!str)
 		return (NULL);
-	index_s1 = 0;
-	index_s2 = 0;
-	while (s1[index_s1])
-	{
-		str[index_s1] = s1[index_s1];
-		index_s1++;
-	}
-	while (s2[index_s2])
-	{
-		str[index_s1 + index_s2] = s2[index_s2];
-		index_s2++;
-	}
-	str[index_s1 + index_s2] = '\0';
+	index = 0;
+	while (s1 && *s1)
+		str[index++] = *s1++;
+	while (s2 && *s2)
+		str[index++] = *s2++;
+	str[index] = '\0';
 	return (str);
-}*/
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	char			*arr;
-	unsigned int	i;
-	unsigned int	total;
-
-	total = nmemb * size;
-	if (nmemb == 0 || size == 0)
-	{
-		nmemb = 1;
-		size = 1;
-	}
-	arr = malloc(total);
-	if (!arr)
-		return (NULL);
-	i = 0;
-	while (i < (nmemb * size))
-	{
-		arr[i] = 0;
-		i++;
-	}
-	return ((void *)arr);
 }
 
-/*size_t	ft_strlen(char *str)
+size_t	ft_strlen(char *str)
 {
 	size_t	i;
-
+	
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
 	return (i);
-}*/
+}
+
+char	*ft_strchr(char *s, int c)
+{
+	char	x;
+	size_t	i;
+
+	i = 0;
+	x = (char) c;
+	while (s[i] != '\0')
+	{
+		if (s[i] == x)
+			return ((char *) s + i);
+		i++;
+	}
+	if (s[i] == x)
+		return ((char *) s + i);
+	return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+	int		i;
+	char	*dest;
+
+	if (s == NULL)
+		return (NULL);
+	dest = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dest[i] = s[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
